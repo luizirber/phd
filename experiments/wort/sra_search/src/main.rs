ed161fca3431d246c2f2f51b00ee3c4785590c36
Luiz Irber <luiz.irber@gmail.com>
2020-09-13 11:00:51 -0700

    upd

--- experiments/wort/sra_search/src/main.rs
+++ experiments/wort/sra_search/src/main.rs
@@ -161,7 +161,10 @@ fn search<P: AsRef<Path>>(
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    env_logger::Builder::from_env(
+        env_logger::Env::default()
+        .default_filter_or("info")
+      ).init();
 
     let opts = Cli::from_args();
 
